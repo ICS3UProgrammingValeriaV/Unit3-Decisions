82c79e19b4b5b2ba9e13e54c5c6dad13ed9121ae
Valeria Veverita <36117142+ValeriaVeverita@users.noreply.github.com>
2018-10-10 10:27:52 -0400

    AddedProgram

---
+++ GuessingValeriaV/GuessingValeriaV/guessingGameForm.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace GuessingValeriaV
+{
+    partial class frmGuess
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblGuess = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblGuess
+            // 
+            this.lblGuess.AutoSize = true;
+            this.lblGuess.Location = new System.Drawing.Point(48, 84);
+            this.lblGuess.Name = "lblGuess";
+            this.lblGuess.Size = new System.Drawing.Size(182, 13);
+            this.lblGuess.TabIndex = 0;
+            this.lblGuess.Text = "Guess an integer between 1 and 10: ";
+            this.lblGuess.Click += new System.EventHandler(this.lblGuess_Click);
+            // 
+            // frmGuess
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(528, 387);
+            this.Controls.Add(this.lblGuess);
+            this.Name = "frmGuess";
+            this.Text = "guessing Game by ";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblGuess;
+    }
+}
+
---
+++ GuessingValeriaV/GuessingValeriaV/guessingGameForm.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GuessingValeriaV
+{
+    public partial class frmGuess : Form
+    {
+        public frmGuess()
+        {
+            InitializeComponent();
+        }
+
+        private void lblGuess_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
