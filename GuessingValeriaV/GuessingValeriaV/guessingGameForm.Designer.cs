78f26f95aae9fc6fc6f34c96bfbceda3293abd76
Valeria Veverita <36117142+ValeriaVeverita@users.noreply.github.com>
2018-10-10 11:04:40 -0400

    CodeChanged

--- GuessingValeriaV/GuessingValeriaV/guessingGameForm.Designer.cs
+++ GuessingValeriaV/GuessingValeriaV/guessingGameForm.Designer.cs
@@ -29,23 +29,47 @@
         private void InitializeComponent()
         {
             this.lblGuess = new System.Windows.Forms.Label();
+            this.txtNumber = new System.Windows.Forms.TextBox();
+            this.btnCheck = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // lblGuess
             // 
             this.lblGuess.AutoSize = true;
-            this.lblGuess.Location = new System.Drawing.Point(48, 84);
+            this.lblGuess.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblGuess.Location = new System.Drawing.Point(12, 115);
             this.lblGuess.Name = "lblGuess";
-            this.lblGuess.Size = new System.Drawing.Size(182, 13);
+            this.lblGuess.Size = new System.Drawing.Size(438, 29);
             this.lblGuess.TabIndex = 0;
-            this.lblGuess.Text = "Guess an integer between 1 and 10: ";
+            this.lblGuess.Text = "Guess an integer between 1 and 15: ";
             this.lblGuess.Click += new System.EventHandler(this.lblGuess_Click);
+            // 
+            // txtNumber
+            // 
+            this.txtNumber.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNumber.Location = new System.Drawing.Point(456, 115);
+            this.txtNumber.Name = "txtNumber";
+            this.txtNumber.Size = new System.Drawing.Size(59, 31);
+            this.txtNumber.TabIndex = 1;
+            // 
+            // btnCheck
+            // 
+            this.btnCheck.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCheck.Location = new System.Drawing.Point(192, 171);
+            this.btnCheck.Name = "btnCheck";
+            this.btnCheck.Size = new System.Drawing.Size(120, 34);
+            this.btnCheck.TabIndex = 2;
+            this.btnCheck.Text = "Check";
+            this.btnCheck.UseVisualStyleBackColor = true;
+            this.btnCheck.Click += new System.EventHandler(this.btnCheck_Click);
             // 
             // frmGuess
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(528, 387);
+            this.Controls.Add(this.btnCheck);
+            this.Controls.Add(this.txtNumber);
             this.Controls.Add(this.lblGuess);
             this.Name = "frmGuess";
             this.Text = "guessing Game by ";
@@ -57,6 +81,8 @@
         #endregion
 
         private System.Windows.Forms.Label lblGuess;
+        private System.Windows.Forms.TextBox txtNumber;
+        private System.Windows.Forms.Button btnCheck;
     }
 }
 
--- GuessingValeriaV/GuessingValeriaV/guessingGameForm.cs
+++ GuessingValeriaV/GuessingValeriaV/guessingGameForm.cs
@@ -7,17 +7,26 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Media;
 
 namespace GuessingValeriaV
 {
     public partial class frmGuess : Form
     {
+        System.Media.SoundPlayer correct = new System.Media.SoundPlayer();
         public frmGuess()
-        {
+        { 
+
+           
             InitializeComponent();
         }
 
         private void lblGuess_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCheck_Click(object sender, EventArgs e)
         {
 
         }
