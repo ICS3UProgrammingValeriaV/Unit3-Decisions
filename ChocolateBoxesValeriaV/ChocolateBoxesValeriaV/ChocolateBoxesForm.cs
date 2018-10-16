8d7bf4a2402b456db9fc77dd4fdd5445395326b2
Valeria Veverita <36117142+ValeriaVeverita@users.noreply.github.com>
2018-10-16 11:05:40 -0400

    Change

---
+++ ChocolateBoxesValeriaV/ChocolateBoxesValeriaV/ChocolateBoxesForm.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ChocolateBoxesValeriaV
+{
+    public partial class ChocolateBoxesForm : Form
+    {
+        public ChocolateBoxesForm()
+        {
+            InitializeComponent();
+            lblPrize.Hide();
+        }
+
+        private void btnCheck_Click(object sender, EventArgs e)
+        {
+            int boxes = int.Parse(txtBoxes.Text);
+
+            if (boxes > 20)
+            {
+                lblPrize.Text = "Prize";
+                lblPrize.Show();
+            }
+            else if (boxes< 10)
+            {
+                lblPrize.Text = "Honorable Mention";
+                lblPrize.Show();
+            }
+            else if (20<boxes && boxes>10)
+            {
+                lblPrize.Text = "Small Prize";
+                lblPrize.Show();
+            }
+            else
+            {
+                lblPrize.Text = "Enter a positive integer";
+                lblPrize.Show();
+            }
+        }
+    }
+}
