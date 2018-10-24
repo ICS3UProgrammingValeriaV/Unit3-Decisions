ab6abd167d230d4d03385c4f6b7a5f5bb0384461
Valeria Veverita <36117142+ValeriaVeverita@users.noreply.github.com>
2018-10-24 11:01:20 -0400

    Change

---
+++ WalkingManValeriaV/WalkingManValeriaV/WalingManForm.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace WalkingManValeriaV
+{
+    partial class frmWalkingMan
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnWalk = new System.Windows.Forms.Button();
+            this.picMan = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.picMan)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnWalk
+            // 
+            this.btnWalk.Font = new System.Drawing.Font("Impact", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnWalk.Location = new System.Drawing.Point(142, 76);
+            this.btnWalk.Name = "btnWalk";
+            this.btnWalk.Size = new System.Drawing.Size(84, 37);
+            this.btnWalk.TabIndex = 0;
+            this.btnWalk.Text = "WALK";
+            this.btnWalk.UseVisualStyleBackColor = true;
+            this.btnWalk.Click += new System.EventHandler(this.btnWalk_Click);
+            // 
+            // picMan
+            // 
+            this.picMan.Image = global::WalkingManValeriaV.Properties.Resources.walk1;
+            this.picMan.Location = new System.Drawing.Point(101, 129);
+            this.picMan.Name = "picMan";
+            this.picMan.Size = new System.Drawing.Size(177, 174);
+            this.picMan.TabIndex = 1;
+            this.picMan.TabStop = false;
+            // 
+            // frmWalkingMan
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(399, 357);
+            this.Controls.Add(this.picMan);
+            this.Controls.Add(this.btnWalk);
+            this.Name = "frmWalkingMan";
+            this.Text = "Walking Man by Valeria V";
+            ((System.ComponentModel.ISupportInitialize)(this.picMan)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnWalk;
+        private System.Windows.Forms.PictureBox picMan;
+    }
+}
+
---
+++ WalkingManValeriaV/WalkingManValeriaV/WalingManForm.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace WalkingManValeriaV
+{
+    public partial class frmWalkingMan : Form
+    {
+        public frmWalkingMan()
+        {
+            InitializeComponent();
+        }
+
+        private void btnWalk_Click(object sender, EventArgs e)
+        {
+            int initialValue = 1;
+            const int MAX_VALUE = 10;
+
+            while (initialValue <= MAX_VALUE)
+            {
+                if (initialValue == 1)
+                {
+                    picMan.Image = Properties.Resources.walk1;
+                }
+                else if (initialValue == 2)
+                {
+                    picMan.Image = Properties.Resources.walk2;
+                }
+                else if (initialValue == 3)
+                {
+                    picMan.Image = Properties.Resources.walk3;
+                }
+                else if (initialValue == 4)
+                {
+                    picMan.Image = Properties.Resources.walk4;
+                }
+                else if (initialValue == 5)
+                {
+                    picMan.Image = Properties.Resources.walk5;
+                }
+                else if (initialValue == 6)
+                {
+                    picMan.Image = Properties.Resources.walk6;
+                }
+                else if (initialValue == 7)
+                {
+                    picMan.Image = Properties.Resources.walk7;
+                }
+                else if (initialValue == 8)
+                {
+                    picMan.Image = Properties.Resources.walk8;
+                }
+                else if (initialValue == 9)
+                {
+                    picMan.Image = Properties.Resources.walk9;
+                }
+                else if (initialValue == 10)
+                {
+                    picMan.Image = Properties.Resources.walk10;
+                }
+
+                initialValue++;
+                Refresh();
+                Thread.Sleep(100);
+            }
+        }
+    }
+}
