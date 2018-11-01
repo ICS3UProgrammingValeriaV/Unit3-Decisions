fc0f371a3ffeccdaa85ace16ca670cfb217dda86
Valeria Veverita <36117142+ValeriaVeverita@users.noreply.github.com>
2018-11-01 10:56:27 -0400

    Change

---
+++ UnicodeValeriaV/UnicodeValeriaV/UnicodeForm.Designer.cs
@@ -0,0 +1,79 @@
+﻿namespace UnicodeValeriaV
+{
+    partial class frmUnicode
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStart = new System.Windows.Forms.Button();
+            this.lstUnicode = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.BackColor = System.Drawing.Color.Olive;
+            this.btnStart.Font = new System.Drawing.Font("Impact", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStart.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnStart.Location = new System.Drawing.Point(106, 69);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(120, 55);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "START";
+            this.btnStart.UseVisualStyleBackColor = false;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // lstUnicode
+            // 
+            this.lstUnicode.BackColor = System.Drawing.Color.White;
+            this.lstUnicode.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lstUnicode.ForeColor = System.Drawing.Color.DarkGoldenrod;
+            this.lstUnicode.FormattingEnabled = true;
+            this.lstUnicode.ItemHeight = 25;
+            this.lstUnicode.Location = new System.Drawing.Point(80, 152);
+            this.lstUnicode.Name = "lstUnicode";
+            this.lstUnicode.Size = new System.Drawing.Size(171, 129);
+            this.lstUnicode.TabIndex = 1;
+            // 
+            // frmUnicode
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(348, 340);
+            this.Controls.Add(this.lstUnicode);
+            this.Controls.Add(this.btnStart);
+            this.Name = "frmUnicode";
+            this.Text = "Unicode by Valeria V";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.ListBox lstUnicode;
+    }
+}
+
---
+++ UnicodeValeriaV/UnicodeValeriaV/UnicodeForm.cs
@@ -0,0 +1,45 @@
+﻿/*
+ * Created by: valeria Veverita
+ * Created on: 01-November-2018
+ * Created for: ICS3U Programming
+ * Daily Assignment – Day 24 - Unicode
+ * This program converts a decimal to a character
+*/
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UnicodeValeriaV
+{
+    public partial class frmUnicode : Form
+    {
+        public frmUnicode()
+        {
+            InitializeComponent();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            //clear the list box 
+            lstUnicode.Items.Clear();
+            //create local variables and constants
+            int initialNum;
+            const int MAX_NUMBER = 90;
+            string actLetter;
+
+            //for initialNum smaller or equal to 90, convert the initialNum to Unicode character
+            for (initialNum = 65; initialNum<=MAX_NUMBER; initialNum ++)
+            {
+                actLetter = Char.ConvertFromUtf32(initialNum);
+                lstUnicode.Items.Add(actLetter + " --> " + Convert.ToString(initialNum));
+                this.Refresh();
+            }
+        }
+    }
+}
