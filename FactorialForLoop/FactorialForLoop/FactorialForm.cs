0b9bd4bb82565e7ad145438e51d07b763ead1c25
Valeria Veverita <36117142+ValeriaVeverita@users.noreply.github.com>
2018-11-01 11:07:12 -0400

    Change

--- FactorialForLoop/FactorialForLoop/FactorialForm.cs
+++ FactorialForLoop/FactorialForLoop/FactorialForm.cs
@@ -30,17 +30,32 @@ namespace FactorialForLoop
         private void btnCalculate_Click(object sender, EventArgs e)
         {
             //create local variables
-            int counter = 1;
+            int counter;
             double sumAnswer = 0;
             double userNumber;
 
             //check if the user answer is a double
             if (Double.TryParse(txtUserChoice.Text, out userNumber))
             {
-                if (userNumber < 0 )
+                //is the user answer is smaller than 0, ask the user to enter a positive number
+                if (userNumber <= 0 )
                 {
-                    lblAnswer.Text = "Enter a positive number";
+                    lblAnswer.Text = "Enter a number bigger than 0";
                 }
+                else if (userNumber > 0)
+                {
+                    for(counter = 1; counter<=userNumber; counter++ )
+                    {
+                        sumAnswer = sumAnswer + counter;
+                        lstNumbers.Items.Add(counter);
+                        this.Refresh();
+                    }
+                    lblAnswer.Text = "Sum of all in" + Convert.ToString(userNumber) + " = " + Convert.ToString(sumAnswer);
+                }
+            }
+            else
+            {
+                lblAnswer.Text = "Enter a number";
             }
         }
     }
