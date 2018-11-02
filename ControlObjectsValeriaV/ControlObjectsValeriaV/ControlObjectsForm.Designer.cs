c2dff8be922acc40a71425962cef2cd460b8ad3f
Valeria Veverita <36117142+ValeriaVeverita@users.noreply.github.com>
2018-11-02 11:06:22 -0400

    Change

---
+++ ControlObjectsValeriaV/ControlObjectsValeriaV/ControlObjectsForm.Designer.cs
@@ -0,0 +1,79 @@
+﻿namespace ControlObjectsValeriaV
+{
+    partial class frmControlObjects
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnColor = new System.Windows.Forms.Button();
+            this.lblColor = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnColor
+            // 
+            this.btnColor.BackColor = System.Drawing.SystemColors.Control;
+            this.btnColor.Font = new System.Drawing.Font("Impact", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnColor.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btnColor.Location = new System.Drawing.Point(100, 71);
+            this.btnColor.Name = "btnColor";
+            this.btnColor.Size = new System.Drawing.Size(114, 44);
+            this.btnColor.TabIndex = 0;
+            this.btnColor.Text = "COLOR";
+            this.btnColor.UseVisualStyleBackColor = false;
+            this.btnColor.Click += new System.EventHandler(this.btnColor_Click);
+            // 
+            // lblColor
+            // 
+            this.lblColor.AutoSize = true;
+            this.lblColor.Font = new System.Drawing.Font("Monospac821 BT", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblColor.Location = new System.Drawing.Point(37, 144);
+            this.lblColor.Name = "lblColor";
+            this.lblColor.Size = new System.Drawing.Size(262, 24);
+            this.lblColor.TabIndex = 1;
+            this.lblColor.Text = "The color has changed";
+            // 
+            // frmControlObjects
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(327, 295);
+            this.Controls.Add(this.lblColor);
+            this.Controls.Add(this.btnColor);
+            this.Name = "frmControlObjects";
+            this.Text = "Control Objects by Valeria V";
+            this.Load += new System.EventHandler(this.frmControlObjects_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnColor;
+        private System.Windows.Forms.Label lblColor;
+    }
+}
+
---
+++ ControlObjectsValeriaV/ControlObjectsValeriaV/ControlObjectsForm.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ControlObjectsValeriaV
+{
+    public partial class frmControlObjects : Form
+    {
+        int counter = 0;
+        public frmControlObjects()
+        {
+            
+            InitializeComponent();
+        }
+
+        private void frmControlObjects_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnColor_Click(object sender, EventArgs e)
+        {
+            
+            counter++;
+
+                if (counter == 1)
+                {
+                    foreach (Control aControlObject in this.Controls)
+                    {
+                        aControlObject.BackColor = Color.DarkGoldenrod;
+                        aControlObject.ForeColor = Color.Cornsilk;
+
+                      if (aControlObject.GetType() == typeof(Label) )
+                      {
+                        aControlObject.BackColor = Color.White;
+                        aControlObject.ForeColor = Color.Black;
+                      }
+                    }
+                }
+                else if (counter == 2)
+                {
+                    foreach (Control aControlObject in this.Controls)
+                    {
+                        aControlObject.BackColor = Color.DarkOliveGreen;
+                        aControlObject.ForeColor = Color.Honeydew;
+                      if (aControlObject.GetType() == typeof(Label))
+                      {
+                        aControlObject.BackColor = Color.White;
+                        aControlObject.ForeColor = Color.Black;
+                      }
+                    }
+                }
+                else if (counter == 3)
+                {
+                    foreach (Control aControlObject in this.Controls)
+                    {
+                        aControlObject.BackColor = Color.DarkSlateGray;
+                        aControlObject.ForeColor = Color.Azure;
+                       if (aControlObject.GetType() == typeof(Label))
+                       {
+                        aControlObject.BackColor = Color.White;
+                        aControlObject.ForeColor = Color.Black;
+                       }
+                    }
+                }
+                else if (counter == 4)
+                {
+                    foreach (Control aControlObject in this.Controls)
+                    {
+                        aControlObject.BackColor = Color.Maroon;
+                        aControlObject.ForeColor = Color.FloralWhite;
+                       if (aControlObject.GetType() == typeof(Label))
+                       {
+                        aControlObject.BackColor = Color.White;
+                        aControlObject.ForeColor = Color.Black;
+                       }
+                    }
+                }
+                else if (counter == 5)
+                {
+                   foreach (Control aControlObject in this.Controls)
+                   {
+                      aControlObject.BackColor = Color.DarkSlateBlue;
+                      aControlObject.ForeColor = Color.GhostWhite;
+                      counter = 0;
+                      if (aControlObject.GetType() == typeof(Label))
+                      {
+                        aControlObject.BackColor = Color.White;
+                        aControlObject.ForeColor = Color.Black;
+                      }
+                   }
+                }
+
+            
+
+        }
+    }
+}
