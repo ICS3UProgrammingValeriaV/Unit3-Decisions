c2b21b43d0409c0dd5a59ff3c668424ea2d9d40d
Valeria Veverita <36117142+ValeriaVeverita@users.noreply.github.com>
2018-11-05 11:05:03 -0500

    Change

---
+++ NestedLoopsValeriaV/NestedLoopsValeriaV/NestedLoopForm.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace NestedLoopsValeriaV
+{
+    partial class frmNestedLoop
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStart = new System.Windows.Forms.Button();
+            this.lstLetters = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.BackColor = System.Drawing.Color.Olive;
+            this.btnStart.Font = new System.Drawing.Font("Impact", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStart.ForeColor = System.Drawing.Color.Cornsilk;
+            this.btnStart.Location = new System.Drawing.Point(103, 38);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(90, 38);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = false;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // lstLetters
+            // 
+            this.lstLetters.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lstLetters.FormattingEnabled = true;
+            this.lstLetters.ItemHeight = 20;
+            this.lstLetters.Location = new System.Drawing.Point(90, 96);
+            this.lstLetters.Name = "lstLetters";
+            this.lstLetters.Size = new System.Drawing.Size(120, 144);
+            this.lstLetters.TabIndex = 1;
+            // 
+            // frmNestedLoop
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(311, 296);
+            this.Controls.Add(this.lstLetters);
+            this.Controls.Add(this.btnStart);
+            this.Name = "frmNestedLoop";
+            this.Text = "Nested Loop by Valeria V";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.ListBox lstLetters;
+    }
+}
+
---
+++ NestedLoopsValeriaV/NestedLoopsValeriaV/NestedLoopForm.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NestedLoopsValeriaV
+{
+    public partial class frmNestedLoop : Form
+    {
+        public frmNestedLoop()
+        {
+            InitializeComponent();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            //create local variables & constants
+            int counterCap, counterLow;
+            string capitalLet, lowerLet;
+            const int MAX_CAP = 90;
+            const int MIN_CAP = 65;
+            const int MAX_LOW = 122;
+            const int MIN_LOW = 97;
+
+            for (counterCap = MIN_CAP; counterCap<=MAX_CAP; counterCap++)
+            {
+                for (counterLow = MIN_LOW; counterLow <= MAX_LOW; counterLow++)
+                {
+                    capitalLet = Char.ConvertFromUtf32(counterCap);
+                    lowerLet = Char.ConvertFromUtf32(counterLow);
+                    lstLetters.Items.Add( capitalLet + "-- >" +  lowerLet);
+                }
+            }
+        }
+    }
+}
